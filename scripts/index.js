380cbbb7543efc855668e05aaaa6d4e0f30ba9f6
Eren Baygun <eren@baygun.net>
2022-07-10 01:31:48 +0300

    github tab style update

--- scripts/index.js
+++ scripts/index.js
@@ -152,8 +152,13 @@ function showGithubRepos() {
       repositoryElement.onclick = () => window.open(repository.html_url, '_blank');
       repositoryElement.innerHTML = `<repo-title>${repository.full_name}</repo-title>
     <description>${repository.description || ''}</description>
-    <p style="margin-top: auto;"><a>Stars:</a> <a>${repository.stargazers_count}</a></p>
-    <p><a>Language:</a> <a tippy="${repository.language}">${properties.languages[`${repository.language}`] || repository.language}</a></p>`;
+    <p style="margin-top: auto;">
+    <a tippy="${repository.language}">${properties.languages[`${repository.language}`] || repository.language}</a>
+    </p>
+    <p>
+    <svg xmlns="http://www.w3.org/2000/svg" height="12" viewBox="0 0 24 24" fill="none" class="sc-bkkeKt cNnlrr"><g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 2l3.09 6.26L22 9.27l-5 4.87l1.18 6.88L12 17.77l-6.18 3.25L7 14.14L2 9.27l6.91-1.01L12 2z"></path></g></svg>
+    <a>${repository.stargazers_count}</a>
+    </p>`;
       $('github').append(repositoryElement);
     });
     $('github').removeAttr('loading');
